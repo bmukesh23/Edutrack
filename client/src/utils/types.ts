5379651ae0135cde591307d0834e1dba428e3fc2
Mukesh <billamukesh1234@gmail.com>
2025-02-10 13:16:41 +0530

    add: types.ts

---
+++ client/src/utils/types.ts
@@ -0,0 +1,24 @@
+export interface UserDetails {
+    email: string;
+    name: string;
+    photoURL: string;
+}
+
+export interface UserPrefDetails {
+    email: string;
+    preferences: {
+        subjects: string;
+        skillLevel: string;
+    };
+}
+
+export interface DashboardUserDetails {
+    name: string;
+    photoURL: string;
+}
+
+export interface Question {
+    question: string;
+    options: string[];
+    answer: string;
+}
